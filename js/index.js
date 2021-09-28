c7f230bc961fda0345f82615da5a6a00934bf82a
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-09-28 13:52:14 -0700

    Added a mouseover effect in index.js onto the navigation anchors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+// Selecting the Navigation Anchors
+const navAnchorsSelector = document.querySelectorAll("nav.nav a.nav-link");
+
+// Disabling default operation on Navigation Anchors AND adding "mouseover" event
+navAnchorsSelector.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+  });
+  link.style.transition = "1s";
+  link.addEventListener("mouseover", function (event) {
+    if (event.target.style.color !== "red") {
+      event.target.style.color = "red";
+    } else {
+      event.target.style.color = "#212529";
+    }
+  });
+});
+
+//
